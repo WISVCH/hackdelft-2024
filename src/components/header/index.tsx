8a3ce5d2c73e08fb82c725674b35931846eb9114
sajochems <scottiej2001@gmail.com>
2023-11-20 22:13:04 +0100

    Change images

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import { h, FunctionalComponent } from "preact";
 
 import * as style from "./style.scss";
 
-import icon from "url:/static/logo_alt.png?as=webp&width=500";
+import icon from "url:/static/HogeResolutieLogo.png?as=webp&width=500";
 
 /**
  * Renders the header of the site.
