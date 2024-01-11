56ed08f47e7ae078aea90290b92f87db3d689672
Marit Radder <maritradder@MacBook-Air-van-Marit.local>
2024-01-11 12:12:44 +0100

    fix timer

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,25 +1,58 @@
 import { h, FunctionalComponent } from "preact";
+import { useEffect, useState } from "preact/hooks";
+import * as style from "./style.scss";
+
 
 
 export const Timer: FunctionalComponent = () => {
-    // const calculateTimeLeft = () => {
-    //     const now = new Date();
-    //     const difference = new Date('2023-12-31T23:59:59');
-    
-    //     let timeLeft = {};
-    
-    //     if (difference > 0) {
-    //       timeLeft = {
-    //         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    //         hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    //         minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
-    //       };
-    //     }
-    
-    //     return timeLeft;
-    //   };
+    const calculateTimeLeft = () => {
+        const difference = (new Date("2024-05-25T13:00:00")).valueOf() - (new Date()).valueOf()
 
-    return(
-        <div> Test </div>
+        let timeLeft = {};
+
+        if (difference > 0) {
+            
+            const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+            setCountdown({
+                days: days,
+                hours: hours,
+                minutes: minutes,
+                seconds: seconds,
+              });
+        } else {
+            setCountdown({
+                days: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+              });
+        }
+    };
+
+    const [countdown, setCountdown] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      });
+
+    useEffect(() => {
+        const interval = setInterval(calculateTimeLeft, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div class={style.timer}>
+            <div>
+                {countdown.days} <span>d</span>
+                {countdown.hours} <span>h</span>
+                {countdown.minutes} <span>m</span>
+                {countdown.seconds} <span>s</span>
+            </div>
+        </div>
     );
 }
